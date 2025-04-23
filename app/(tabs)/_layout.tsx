015aee2943a786cb6ed8623ea4c35fd093a80945
zxxx98 <zx156406687@gmail.com>
2025-04-23 22:52:43 +0800

    feat: 添加iOS配置和优化标签布局
    - 在app.json中新增iOS相关配置，包括bundleIdentifier和infoPlist设置。
    - 在package.json中添加打开Android模拟器的脚本命令。
    - 优化_tab_layout.tsx文件，调整标签数据结构，新增添加行为标签，并改善标签样式以提升用户体验。

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,8 +4,7 @@ import { StyleSheet } from 'react-native';
 import React from 'react';
 
 // 定义标签数据接口
-interface TabDataItem
-{
+interface TabDataItem {
     name: string;
     title: string;
     iconName: string;
@@ -16,12 +15,13 @@ interface TabDataItem
 const tabData: TabDataItem[] = [
     { name: 'timelinePage', title: '时间线', iconName: 'clock-outline' },
     { name: 'todoPage', title: '待办', iconName: 'list-outline' },
+    { name: 'addActionPage', title: '', iconName: 'plus-outline' },
     { name: 'plantsPage', title: '花园', iconName: 'flower-outline', iconPack: 'ionicons' },
     { name: 'settingsPage', title: '设置', iconName: 'settings-2-outline' }
 ];
 
-export default function TabLayout()
-{
+
+export default function TabLayout() {
     return (
         <Tabs
             screenOptions={{
@@ -31,28 +31,39 @@ export default function TabLayout()
             tabBar={(props: any) => (
                 <BottomNavigation
                     selectedIndex={props.state.index}
-                    onSelect={index =>
-                    {
-                        props.navigation.navigate(props.state.routeNames[index]);
+                    onSelect={index => {
+                        if (index === 2) {
+                            //打开添加行为面板
+                        } else {
+                            props.navigation.navigate(props.state.routeNames[index]);
+                        }
                     }}
                     appearance='noIndicator'
                     style={{ height: 60 }}
                 >
-                    {tabData.map((tab, index) => (
-                        <BottomNavigationTab
-                            key={tab.name}
-                            icon={(props) => (
-                                <Icon
-                                    {...props}
-                                    pack={tab.iconPack || 'eva'}
-                                    name={tab.iconName}
-                                    animation={'pulse'} // 使用UI Kitten内置的animation属性
-                                />
-                            )}
-                            title={tab.title}
-                            style={props.state.index === index ? styles.activeTab : styles.inactiveTab}
-                        />
-                    ))}
+                    {tabData.map((tab, index) => {
+                        const realIndex = props.state.index > 1 ? props.state.index + 1 : props.state.index;
+                        let style:any = props.state.index === index ? styles.activeTab : styles.inactiveTab;
+                        if(index === 2)
+                        {
+                            //  Object.assign(style, styles.circleTab);
+                            style = styles.circleTab;
+                        }
+                        return (
+                            <BottomNavigationTab
+                                key={tab.name}
+                                icon={(props) => (
+                                    <Icon
+                                        {...props}
+                                        pack={tab.iconPack || 'eva'}
+                                        name={tab.iconName}
+                                    />
+                                )}
+                                title={tab.title}
+                                style={style}
+                            />
+                        )
+                    })}
                 </BottomNavigation>
             )}
         >
@@ -72,4 +83,18 @@ const styles = StyleSheet.create({
         opacity: 0.5,
         flex: 1,
     },
+    circleTab: {
+        height: 40,
+        borderRadius: 15,
+        backgroundColor: '#2B3A67',  // 深色背景
+        margin: 8,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    }
 });
---
+++ app/(tabs)/addActionPage.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+
+const AddActionPage = () => {
+    return (
+        <View>
+            123
+        </View>
+    )
+}
+
+export default AddActionPage;
